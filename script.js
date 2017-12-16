694a95d9d653460575152d67dba67e04ecceceda
frobino <frobino@users.noreply.github.com>
2017-12-16 11:52:20 +0100

    step 5

--- script.js
+++ script.js
@@ -67,8 +67,16 @@ function ballCollisionCheck(){
   if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
     dx = -dx;
   }
-  if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+  if(y + dy < ballRadius) {
     dy = -dy;
+  } else if(y + dy > canvas.height-ballRadius) {
+    if(x > paddleX && x < paddleX + paddleWidth) {
+        dy = -dy;
+    }
+    else {
+        alert("GAME OVER");
+        document.location.reload();
+    }
   }
 }
 
@@ -76,9 +84,18 @@ function sausageCollisionCheck(){
   if(x + dx > canvas.width-image.width || x + dx < 0) {
     dx = -dx;
   }
-  if(y + dy > canvas.height || y + dy < 0 + image.height) {
+  if(y + dy < 0 + image.height) {
     dy = -dy;
+  } else if(y + dy > canvas.height) {
+    if(x > paddleX - 5 && x < paddleX + paddleWidth + 5) {
+        dy = -dy;
+    }
+    else {
+        alert("KORV OVER");
+        document.location.reload();
+    }
   }
+
 }
 
 function drawBlueBall(){
