1188920e950635b1a17500c5cfcb635a7df9890b
frobino <frobino@users.noreply.github.com>
2017-12-30 21:35:54 +0100

    [platformer] refactor Hero class to own js file

--- platformer/js/main.js
+++ platformer/js/main.js
@@ -65,19 +65,6 @@ PlayState._spawnCharacters = function (data) {
 };
 
 /**
- * Hero object, deriving from Phaser.Sprite
- */
-function Hero(game, x ,y) {
-  // call Phaser.Sprite constructor
-  Phaser.Sprite.call(this, game, x, y, 'hero');
-  this.anchor.set(0.5, 0.5);
-};
-
-// inherit from Phaser.Sprite
-Hero.prototype = Object.create(Phaser.Sprite.prototype);
-Hero.prototype.constructor = Hero;
-
-/**
  * MAIN
  */
 window.onload = function() {
