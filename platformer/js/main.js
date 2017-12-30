990c5075186d70feb58af86980b9268510438c0c
frobino <frobino@users.noreply.github.com>
2017-12-30 16:38:17 +0100

    [platformer] add hero sprite
    Includes example of derivation in js classes

--- platformer/js/main.js
+++ platformer/js/main.js
@@ -28,6 +28,8 @@ PlayState.preload = function() {
   this.game.load.image('grass:4x1', 'images/grass_4x1.png');
   this.game.load.image('grass:2x1', 'images/grass_2x1.png');
   this.game.load.image('grass:1x1', 'images/grass_1x1.png');
+  // preload image hero
+  this.game.load.image('hero', 'images/hero_stopped.png');
 }
 
 // Create
@@ -44,12 +46,36 @@ PlayState._loadlevel = function(data) {
 
   // spawn all platforms
   data.platforms.forEach(this._spawnPlatform, this);
+
+  //...
+  // spawn hero and enemies
+  this._spawnCharacters({hero: data.hero});
 }
 
 // _spawnPlatform helper
 PlayState._spawnPlatform = function(platform){
   this.game.add.sprite(platform.x, platform.y, platform.image);
 }
+
+// _spawnCharacters helper
+PlayState._spawnCharacters = function (data) {
+    // spawn hero
+    this.hero = new Hero(this.game, data.hero.x, data.hero.y);
+    this.game.add.existing(this.hero);
+};
+
+/**
+ * Hero object, deriving from Phaser.Sprite
+ */
+function Hero(game, x ,y) {
+  // call Phaser.Sprite constructor
+  Phaser.Sprite.call(this, game, x, y, 'hero');
+  this.anchor.set(0.5, 0.5);
+};
+
+// inherit from Phaser.Sprite
+Hero.prototype = Object.create(Phaser.Sprite.prototype);
+Hero.prototype.constructor = Hero;
 
 /**
  * MAIN
